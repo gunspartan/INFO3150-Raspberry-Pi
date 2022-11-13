0be5739b860c4b5d23064d24dfbe987773267854
sunaboi <gunhwang99@gmail.com>
2022-11-13 00:11:18 -0800

    initial commit

---
+++ gpioController.py
@@ -0,0 +1,51 @@
+from gpiozero import LED, Button
+import time
+from signal import pause
+
+class gpioController:
+    def __init__(self):
+        self.COUNT = 5
+        self.DELAY = 0.5
+        self.red = LED(6)
+        self.green = LED(19)
+        self.denyBtn = Button(22)
+        self.allowBtn = Button(17)
+
+    def allowEntryLED(self):
+        for i in range(self.COUNT):
+            self.green.on()
+            time.sleep(self.DELAY)
+            self.green.off()
+            time.sleep(self.DELAY)
+
+    def denyEntryLED(self):
+        for i in range(self.COUNT):
+            self.red.on()
+            time.sleep(self.DELAY)
+            self.red.off()
+            time.sleep(self.DELAY)
+
+    def approveManual(self):
+       print("waiting for manual approval")
+       if self.allowBtn.wait_for_press(5):
+            self.allowEntryLED()
+            print("button pressed")
+       else:
+            print("button not pressed")
+
+    def denyManual(self):
+       print("waiting for manual denial")
+       if self.denyBtn.wait_for_press(5):
+            self.denyEntryLED()
+            print("button pressed")
+       else:
+            print("button not pressed")
+# test
+test = gpioController()
+#print("allow entry")
+#test.allowEntryLED()
+#print("deny entry")
+#test.denyEntryLED()
+
+test.approveManual()
+test.denyManual()
